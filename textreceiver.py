0190fc140e4ffe30dc823313dcf182a5e32dd545
SAVIO PRINCE <69968870+OIVAS7572@users.noreply.github.com>
2021-04-17 19:38:29 +0530

    Update textreceiver.py

--- textreceiver.py
+++ textreceiver.py
@@ -18,6 +18,9 @@ def incoming_sms():
 logging.basicConfig(level=logging.DEBUG, filename='log', filemode='a+', format='%(asctime)s %(message)s')
 
 port = int(os.environ['PORT'])
+host = int(os.environ['HOST'])
+
+print(port, host)
 
 if __name__ == "__main__":
 	app.run(debug=True, port=port)
