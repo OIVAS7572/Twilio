11664968ad788a94a611da35d2b1668144eeb402
SAVIO PRINCE <69968870+OIVAS7572@users.noreply.github.com>
2021-04-17 11:41:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+import logging
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+
+def incoming_sms():
+	body = request.values.get('Body', None)
+	str(body)
+	resp = MessagingResponse()
+	logging.debug(body)
+	resp.message("Hello world!")
+	return str(resp)
+
+logging.basicConfig(level=logging.DEBUG, filename='log', filemode='a+', format='%(asctime)s %(message)s')
+
+if __name__ == "__main__":
+	app.run(debug=True)
